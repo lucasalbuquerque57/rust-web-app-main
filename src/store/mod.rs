7054722c5f98004835b1775e5dfe91a34a9c3075
Lucas Albuquerque <75431427+lucasalbuquerque57@users.noreply.github.com>
2024-12-21 17:20:11 -0300

    Update

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,4 +1,5 @@
 /*!
+
 Transactional value storage.
 
 This module sketches out a storage API that uses transactions to coordinate updates to
@@ -6,6 +7,7 @@ independent data stores. The design assumes data for a given transaction will be
 observable (such as being written to disk or some external database) before the transaction
 itself is committed. The transaction store keeps track of whether or not the data associated
 with a given transaction should be surfaced to callers or not.
+
 */
 
 mod transaction;
